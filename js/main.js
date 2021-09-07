1b096760d4a7083a76f29596e7f9653e7756bf9d
Henry Lofiego <henrylofiego@gmail.com>
2021-09-06 22:45:36 -0400

    fix main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// MFP Init
-$(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
-});
-
-$(document).ready(function() {
-  $(".dropdown-toggle").dropdown();
-});
-
-
 // Preloader
 $( window ).on( "load", function() {
   $('.preloader-container').fadeOut('slow')
